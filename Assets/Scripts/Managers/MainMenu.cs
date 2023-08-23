98d4249c6a6e22b8e5793f64dfcbe7f5d6f13e39
OzanVolkan <100294574+OzanVolkan@users.noreply.github.com>
2023-08-23 23:35:40 +0300

    Fade Out at Loading Scene

--- Assets/Scripts/Managers/MainMenu.cs
+++ Assets/Scripts/Managers/MainMenu.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] Image img;
     public void Play()
     {
-        SceneManager.LoadScene(2);
+        StartCoroutine(FadeOut(2));
     }
 
     public void Quit()
     {
         Debug.Log("Exiting...");
         Application.Quit();
+    }
+
+    IEnumerator FadeOut(int _scene)
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(_scene);
     }
 }
